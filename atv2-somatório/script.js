ac083e8d333d1ddda01111727826c4dae672f7f3
DenisMec9 <denisfilho2@gmail.com>
2024-08-23 23:07:39 +0000

    as

--- atv2-somatório/script.js
+++ atv2-somatório/script.js
@@ -1,11 +1,12 @@
 let somatorio=0;
+const btAdic = document.getElementById("btAdic");
+const inputNum= document.getElementById("inputNum");
+const inputSoma= document.getElementById("btAdic");
 
 function adicionarNumero() {
     const numero= parseFloat(document.getElementById('numero').value);
     if (!isNaN(numero)){
         somatorio += numero;
-        document.getElementById('total').innerText = `somatório: ${somatorio}`;
-        document.getElementById('numero').value ='';
     } else {
         alert('por favor, insira um número inteiro.');
     }
