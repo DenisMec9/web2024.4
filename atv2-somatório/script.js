695cca28dd60338c69ca039decab1cb1e17c0a6a
DenisMec9 <denisfilho2@gmail.com>
2024-08-30 21:45:09 +0000

    2.0

--- atv2-somatório/script.js
+++ atv2-somatório/script.js
@@ -1,13 +1,30 @@
-let somatorio=0;
-const btAdic = document.getElementById("btAdic");
-const inputNum= document.getElementById("inputNum");
-const inputSoma= document.getElementById("btAdic");
+//let somatorio=0;
+//const btAdic = document.getElementById("btAdic");
+//const inputNum= document.getElementById("inputNum");
+//const inputSoma= document.getElementById("btAdic");
 
-function adicionarNumero() {
-    const numero= parseFloat(document.getElementById('numero').value);
-    if (!isNaN(numero)){
-        somatorio += numero;
-    } else {
-        alert('por favor, insira um número inteiro.');
-    }
-}
+//function adicionarNumero() {
+    //const numero= parseFloat(document.getElementById('numero').value);
+    //if (!isNaN(numero)){
+       // somatorio += numero;
+    //} else {
+      //  alert('por favor, insira um número inteiro.');
+   // }
+//}
+const btAdic = document.getElementById("btAdic");
+const inputNum = document.getElementById("inputNum");
+const inputSoma = document.getElementById("inputSoma");
+let somatorio = 0;
+
+btAdic.onclick = () => {
+  let num = parseInt(inputNum.value);
+  if (isNaN(num)) {
+    alert("Digite um número inteiro!");
+    return;
+  }
+
+  somatorio += num;
+  inputSoma.value = somatorio;
+  inputNum.value = "";
+  inputNum.focus();
+};
