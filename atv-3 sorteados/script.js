8784436e8f8c29bfa55a93cfeb27a95397383ca8
DenisMec9 <denisfilho2@gmail.com>
2024-09-05 22:49:24 +0000

    a

--- atv-3 sorteados/script.js
+++ atv-3 sorteados/script.js
@@ -4,27 +4,27 @@ function transformarEmJSON() {
     const idade = document.getElementById('idade').value;
     const cpf = document.getElementById('cpf').value;
 
-    // Validação dos campos
+   
     if (!mat || !nome || !idade || !cpf) {
         alert('Por favor, preencha todos os campos.');
         return;
     }
 
-    // Validação da idade
+    
     if (!Number.isInteger(Number(idade))) {
         alert('Idade deve ser um valor inteiro.');
         return;
     }
 
-    // Criar objeto aluno
+    
     const aluno = {
         mat: mat,
         nome: nome,
-        idade: parseInt(idade, 10), // Converter idade para número inteiro
+        idade: parseInt(idade, 10), 
         cpf: cpf
     };
 
-    // Exibir o objeto aluno em formato JSON
+    
     document.getElementById('resultado').textContent = JSON.stringify(aluno, null, 2);
 }
 
