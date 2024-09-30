e5bd4901670119b3e834a8460e35207d361fd2a6
DenisMec9 <denisfilho2@gmail.com>
2024-09-30 22:43:41 +0000

    3 api ok

--- atv7-Api/api2/script.js
+++ atv7-Api/api2/script.js
@@ -1,32 +1,12 @@
-const API_TOKEN = 'seu_token_aqui';
- 
-        
-        async function fetchPlayerData(playerTag) {
-            const url = `https://api.clashroyale.com/v1/players/%23${playerTag}`;
-            const response = await fetch(url, {
-                headers: {
-                    'Authorization': `Bearer ${API_TOKEN}`
-                }
-            });
- 
-            if (response.ok) {
-                const playerData = await response.json();
-                displayPlayerData(playerData);
-            } else {
-                document.getElementById('player-info').innerText = 'Erro ao buscar dados do jogador.';
-            }
-        }
- 
-       
-        function displayPlayerData(data) {
-            const playerInfoDiv = document.getElementById('player-info');
-            playerInfoDiv.innerHTML = `
-<h2>${data.name}</h2>
-<p>Tag: ${data.tag}</p>
-<p>Nível: ${data.expLevel}</p>
-<p>Troféus: ${data.trophies}</p>
-            `;
-        }
- 
-     
-        fetchPlayerData('2PP'); 
+async function getJoke() {
+    const jokeContainer = document.getElementById('joke');
+    jokeContainer.innerHTML = 'Carregando...';
+    
+    try {
+        const response = await fetch('https://geek-jokes.sameerkumar.website/api?format=json');
+        const data = await response.json();
+        jokeContainer.innerHTML = data.joke;
+    } catch (error) {
+        jokeContainer.innerHTML = 'Erro ao carregar piada. Tente novamente.';
+    }
+}
