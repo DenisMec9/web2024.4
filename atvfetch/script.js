eeedaf8fbed4924d13b92095c03807c919fbafcd
DenisMec9 <denisfilho2@gmail.com>
2024-10-01 22:20:42 +0000

    desenvolvimendo da lista com back-end

--- atvfetch/script.js
+++ atvfetch/script.js
@@ -1,34 +1,43 @@
-const inputID = document.getElementById("inputID");
-const btFetch = document.getElementById("btFetch");
-const prePersonagem = document.getElementById("prePersonagem");
+const olTarefas = document.getElementById("olTarefas");
 
-const handleBtFetchClick = () => {
-  const id = inputID.value.trim();
-  if (!id) {
-    alert("Precisa digitar o ID!");
-    return;
-  }
-
-  fetch(`https://swapi.dev/api/people/${id}`)
-    .then((response) => {
-      console.log(response);
-      if (response.ok) {
-        console.log("Sucesso!");
-      } else {
-        console.log("Erro: ", response.status, response.statusText);
-        alert("Erro ao acessar o servidor - ID inválido?");
-        throw new Error("Erro encontrado: " + response.status);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("data", data);
-      // prePersonagem.innerHTML = JSON.stringify(data);
-      prePersonagem.innerHTML = `${data.name} - ${data.birth_year}`;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+const createList = (data) => {
+  olTarefas.innerHTML = "";
+  const tarefas = data.results;
+  tarefas.forEach((tarefa) => {
+    const text = document.createTextNode(`${tarefa.descricao} `);
+    const li = document.createElement("li");
+    li.appendChild(text);
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
+    cb.checked = tarefa.concluida;
+    cb.disabled = true;
+    li.appendChild(cb);
+    olTarefas.appendChild(li);
+  });
 };
 
-btFetch.onclick = handleBtFetchClick;
+const getTarefas = async () => {
+  try {
+    const response = await fetch(
+      "https://parseapi.back4app.com/classes/Tarefa",
+      {
+        method: "GET",
+        headers: {
+          "X-Parse-Application-Id": "4cRNaPFAu9tGO59OrBIJpH4v6qeHUUbtReuVmjP7",
+          "X-Parse-REST-API-Key": "x3OZm4ZBkTNLy3jJUc9JImiuAi6XK4ih7zfKOVwk",
+        },
+      }
+    );
+    console.log(response);
+    if (!response.ok) {
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+    const data = await response.json();
+    createList(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+window.onload = getTarefas;
