99d988b1e7745915942c428a77824cc3d08d4325
DenisMec9 <denisfilho2@gmail.com>
2024-10-04 22:45:21 +0000

    teste 1

---
+++ atv8- CRUD de Despesas/script.js
@@ -0,0 +1,5 @@
+const olTarefas = document.getElementById("Despesas");
+const inputDescricao = document.getElementById("inputDescricao");
+const btAdicionar = document.getElementById("btAdicionar");
+
+const baseURL = "";
