346015fb33860d0d5befe82e09eb8cf61bb00018
DenisMec9 <denisfilho2@gmail.com>
2024-10-07 23:03:56 +0000

    teste fail

--- atv8- CRUD de Despesas/script.js
+++ atv8- CRUD de Despesas/script.js
@@ -1,5 +1,33 @@
-const olTarefas = document.getElementById("Despesas");
-const inputDescricao = document.getElementById("inputDescricao");
+const olDespesas= document.getElementById("Despesas");
+const Descricao = document.getElementById("Descricao");
+const valor = document.getElementById("valor");
 const btAdicionar = document.getElementById("btAdicionar");
 
-const baseURL = "";
+const baseURL = "https://parseapi.back4app.com/classes/despesa";
+const headers = {
+    "X-Parse-Application-Id: WOf1KY10sPkIzZxynvsyD8xKO8VxYYpU0b0qpV7F",
+    "X-Parse-REST-API-Key: eppqhzHQDef4zXFtqlgZV5okC3JQuDeP7fCPTiPB"
+  };
+  const headersJson = {
+    ...headers,
+    "Content-Type": "application/json",
+  };
+  const createList = (data) => {
+    Despesas.innerHTML = "";
+    const Descricao = data.results;
+    Descricao.forEach((descricao) => {
+      const text = document.createTextNode(`${Despesas.descricao} `);
+      const li = document.createElement("li");
+      li.appendChild(text);
+      const cb = document.createElement("input");
+      cb.type = "checkbox";
+      cb.checked = tarefa.concluida;
+      cb.onchange = () => handleCheckboxClick(cb, Despesas);
+      li.appendChild(cb);
+      const bt = document.createElement("button");
+      bt.innerHTML = "x";
+      bt.onclick = () => handleBtRemoverClick(bt, Despesas);
+      li.appendChild(bt);
+      olTarefas.appendChild(li);
+    });
+  };
