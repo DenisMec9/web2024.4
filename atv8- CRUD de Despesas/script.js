c97bbf9ca9906d7ec0ce4bb5a0ccf536833c1890
DenisMec9 <denisfilho2@gmail.com>
2024-10-08 22:01:00 +0000

    teste para funcionamento

--- atv8- CRUD de Despesas/script.js
+++ atv8- CRUD de Despesas/script.js
@@ -1,6 +1,6 @@
 const olDespesas= document.getElementById("Despesas");
-const Descricao = document.getElementById("Descricao");
-const valor = document.getElementById("valor");
+const inputDescricao = document.getElementById("Descricao");
+const inputValor = document.getElementById("Valor");
 const btAdicionar = document.getElementById("btAdicionar");
 
 const baseURL = "https://parseapi.back4app.com/classes/despesa";
@@ -12,14 +12,109 @@ const headers = {
     ...headers,
     "Content-Type": "application/json",
   };
-  const createList = (data) => {
-    olDespesas.innerHTML = "";
-    const Descricao = data.results;
-    Descricao.forEach((descricao) => {
-      const text = document.createTextNode(`${Despesas.descricao} `);
-      const li = document.createElement("li");
-      li.appendChild(text);
-      const cb = document.createElement("input");
+const createList = (data) => {
+  olDespesas.innerHTML = "";
+  const despesa = data.results;
+  despesa.forEach((despesa) => {
+    const text = document.createTextNode(`${tarefa.descricao} `);
+    const li = document.createElement("li");
+    li.appendChild(text);
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
+    cb.checked = despesa.concluida;
+    cb.onchange = () => handleCheckboxClick(cb, despesa);
+    li.appendChild(cb);
+    const bt = document.createElement("button");
+    bt.innerHTML = "x";
+    bt.onclick = () => handleBtRemoverClick(bt, despesa);
+    li.appendChild(bt);
+    olDespesas.appendChild(li);
+  });
+};
 
+const handleCheckboxClick = async (cb, despesa) => {
+  try {
+    cb.disabled = true;
+    const response = await fetch(`${baseURL}/${despesa.objectId}`, {
+      method: "PUT",
+      headers: headersJson,
+      body: JSON.stringify({ concluida: !tarefa.concluida }),
     });
-  };
+    cb.disabled = false;
+    console.log(response);
+    if (!response.ok) {
+      cb.checked = !cb.checked;
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+  } catch (error) {
+    cb.checked = !cb.checked;
+    console.log(error);
+  }
+};
+
+const handleBtRemoverClick = async (bt, despesa) => {
+  try {
+    bt.disabled = true;
+    const response = await fetch(`${baseURL}/${despesa.objectId}`, {
+      method: "DELETE",
+      headers: headers,
+    });
+    bt.disabled = false;
+    console.log(response);
+    if (!response.ok) {
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+    getDespesas();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getDespesas = async () => {
+  try {
+    const response = await fetch(baseURL, {
+      method: "GET",
+      headers: headers,
+    });
+    console.log(response);
+    if (!response.ok) {
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+    const data = await response.json();
+    createList(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const handleBtAdicionarClick = async () => {
+  const descricao = inputDescricao.value.trim();
+  if (!descricao) {
+    alert("Necessário adicionar uma descrição para criar a tarefa!");
+    inputDescricao.focus();
+    return;
+  }
+  try {
+    const response = await fetch(baseURL, {
+      method: "POST",
+      headers: headersJson,
+      body: JSON.stringify({ descricao: descricao }),
+    });
+    console.log(response);
+    if (!response.ok) {
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+    inputDescricao.value = "";
+    inputDescricao.focus();
+    getTarefas();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+window.onload = getTarefas;
+btAdicionar.onclick = handleBtAdicionarClick;
