42fca9bd827eac861a0396d0604eda580afeb720
DenisMec9 <denisfilho2@gmail.com>
2024-10-10 01:36:48 +0000

    api back4app

--- atv8- CRUD de Despesas/script.js
+++ atv8- CRUD de Despesas/script.js
@@ -1,120 +1,117 @@
-const olDespesas= document.getElementById("Despesas");
-const inputDescricao = document.getElementById("Descricao");
-const inputValor = document.getElementById("Valor");
-const btAdicionar = document.getElementById("btAdicionar");
+// Getting the elements
+const input_descricao = document.getElementById('input-descricao');
+const input_valor = document.getElementById('input-valor');
+const submit_despesa = document.getElementById('submit-despesa');
+const total_el = document.getElementById('total');
 
-const baseURL = "https://parseapi.back4app.com/classes/despesa";
-const headers = {
-    "X-Parse-Application-Id": "WOf1KY10sPkIzZxynvsyD8xKO8VxYYpU0b0qpV7F",
-    "X-Parse-REST-API-Key": "eppqhzHQDef4zXFtqlgZV5okC3JQuDeP7fCPTiPB",
-  };
-  const headersJson = {
-    ...headers,
-    "Content-Type": "application/json",
-  };
-const createList = (data) => {
-  olDespesas.innerHTML = "";
-  const despesa = data.results;
-  despesa.forEach((despesa) => {
-    const text = document.createTextNode(`${tarefa.descricao} `);
-    const li = document.createElement("li");
-    li.appendChild(text);
-    const cb = document.createElement("input");
-    cb.type = "checkbox";
-    cb.checked = despesa.concluida;
-    cb.onchange = () => handleCheckboxClick(cb, despesa);
-    li.appendChild(cb);
-    const bt = document.createElement("button");
-    bt.innerHTML = "x";
-    bt.onclick = () => handleBtRemoverClick(bt, despesa);
-    li.appendChild(bt);
-    olDespesas.appendChild(li);
-  });
-};
+// Configs
+const url = 'https://parseapi.back4app.com/classes/Despesas';
 
-const handleCheckboxClick = async (cb, despesa) => {
-  try {
-    cb.disabled = true;
-    const response = await fetch(`${baseURL}/${despesa.objectId}`, {
-      method: "PUT",
-      headers: headersJson,
-      body: JSON.stringify({ concluida: !tarefa.concluida }),
-    });
-    cb.disabled = false;
-    console.log(response);
-    if (!response.ok) {
-      cb.checked = !cb.checked;
-      alert("Erro ao acessar o servidor: " + response.status);
-      throw new Error("Erro encontrado: " + response.status);
+// Main function ==================
+async function mainFunction(){
+    try{
+        await loadDespesas(url);
+    } catch(err){
+        throw new Error(err);
     }
-  } catch (error) {
-    cb.checked = !cb.checked;
-    console.log(error);
-  }
-};
+    // Click event add 'despesa'
+    submit_despesa.addEventListener('click', () => {
+        const descricao = input_descricao.value;
+        const valor = Number(input_valor.value);
 
-const handleBtRemoverClick = async (bt, despesa) => {
-  try {
-    bt.disabled = true;
-    const response = await fetch(`${baseURL}/${despesa.objectId}`, {
-      method: "DELETE",
-      headers: headers,
+        try{
+            (async function(){
+                await postDespesa(url, descricao, valor);
+                location.reload();
+            })();
+        } catch(err){
+            throw new Error(err);
+        }
     });
-    bt.disabled = false;
-    console.log(response);
-    if (!response.ok) {
-      alert("Erro ao acessar o servidor: " + response.status);
-      throw new Error("Erro encontrado: " + response.status);
-    }
-    getDespesas();
-  } catch (error) {
-    console.log(error);
-  }
-};
+}
 
-const getDespesas = async () => {
-  try {
-    const response = await fetch(baseURL, {
-      method: "GET",
-      headers: headers,
+// Load despesas
+const loadDespesas = async(url) => {
+    // Call GET request
+    const despesasData = await getRequest(url);
+    let total_valor = 0;
+    // loop to create all 'despesas'
+    despesasData.forEach((despesa) => {
+        const objectId = despesa.objectId;
+        const descricao = despesa.descricao;
+        const valor = despesa.valor;
+        
+        total_valor += Number(valor);
+        // Creating 'despesa' elements
+        createElements(descricao, valor, objectId);
     });
-    console.log(response);
-    if (!response.ok) {
-      alert("Erro ao acessar o servidor: " + response.status);
-      throw new Error("Erro encontrado: " + response.status);
-    }
-    const data = await response.json();
-    createList(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
+    total_el.textContent = (`Total: R$ ${total_valor.toFixed(2)}`);
+}
 
-const handleBtAdicionarClick = async () => {
-  const descricao = inputDescricao.value.trim();
-  if (!descricao) {
-    alert("Necessário adicionar uma descrição para criar a lista despesa!");
-    inputDescricao.focus();
-    return;
-  }
-  try {
-    const response = await fetch(baseURL, {
-      method: "POST",
-      headers: headersJson,
-      body: JSON.stringify({ descricao: descricao }),
+// Requests ==================
+
+// GET request
+const getRequest = async(url) => {
+    const rawResponse = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'X-Parse-Application-Id': 'GZtWVlIIfdvSS2MOlDVleERXdn96mg1An1wrgGWy',
+            'X-Parse-REST-API-Key': 'RCJqIyCphMIIEprwgPqicixMIY9zaTUIzBgbvivv'
+        }
     });
-    console.log(response);
-    if (!response.ok) {
-      alert("Erro ao acessar o servidor: " + response.status);
-      throw new Error("Erro encontrado: " + response.status);
-    }
-    inputDescricao.value = "";
-    inputDescricao.focus();
-    getTarefas();
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-window.onload = getDespesas;
-btAdicionar.onclick = handleBtAdicionarClick;                                                   
+    const dataJson = await rawResponse.json();
+    const despesasData = dataJson.results;
+    
+    return despesasData;
+}
+
+// PUT request
+const putRequest = async(url, id, new_descricao, new_valor) => {
+    await fetch((`${url}/${id}`), {
+        method: 'PUT',
+        headers: {
+            'X-Parse-Application-Id': 'GZtWVlIIfdvSS2MOlDVleERXdn96mg1An1wrgGWy',
+            'X-Parse-REST-API-Key': 'RCJqIyCphMIIEprwgPqicixMIY9zaTUIzBgbvivv',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            descricao: new_descricao,
+            valor: new_valor
+        })
+    });
+}
+
+// DELETE request
+const deleteRequest = async(url, id) => {
+    await fetch((`${url}/${id}`), {
+        method: 'DELETE',
+        headers: {
+            'X-Parse-Application-Id': 'GZtWVlIIfdvSS2MOlDVleERXdn96mg1An1wrgGWy',
+            'X-Parse-REST-API-Key': 'RCJqIyCphMIIEprwgPqicixMIY9zaTUIzBgbvivv'
+        },
+        body: JSON.stringify({})
+    });
+}
+
+// POST request
+const postDespesa = async(url, input_descricao, input_valor) => {
+    const rawResponse = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'X-Parse-Application-Id': 'GZtWVlIIfdvSS2MOlDVleERXdn96mg1An1wrgGWy',
+            'X-Parse-REST-API-Key': 'RCJqIyCphMIIEprwgPqicixMIY9zaTUIzBgbvivv',
+            'Content-Type': 'application/json'
+        },
+        // Just allows in string data type
+        body: JSON.stringify({
+            descricao: input_descricao,
+            valor: input_valor
+        })
+    });
+
+    console.log(rawResponse);
+}
+
+// Call main function
+mainFunction();
