51f66f1df786cc80f60e3da91b26ee19a1f72e00
DenisMec9 <denisfilho2@gmail.com>
2024-11-01 22:58:12 +0000

    ok

--- atv-mentoria /script.js
+++ atv-mentoria /script.js
@@ -11,3 +11,31 @@ function gravar(){
  
     alert('Dados enviados com sucesso')
 }
+function exibir(){
+    let nome = localStorage.getItem("nome");
+/// buscando o localStorage o valor do campo nome e adicionar na variavel 'nome'
+    let endereco = localStorage.getItem("endereco");
+    let email = localStorage.getItem("email");
+    let telefone = localStorage.getItem("telefone");
+    
+    if(nome && endereco && email && telefone){
+        document.getElementById('dados-nome').querySelector('p').textContent = nome;
+        /// adicionando o valor da variavel nome no campo 'P' da Div com id 'dados-nome'
+        document.getElementById('dados-endereco').querySelector('p').textContent = endereco;
+        document.getElementById('dados-email').querySelector('p').textContent = email;
+        document.getElementById('dados-telefone').querySelector('p').textContent = telefone;
+    } else {
+        document.getElementById('dados-exibidos').innerHTML = '<p>Nenhum dado encontrado</p>'
+    }
+}
+function enviar(){
+    let nome = localStorage.getItem("nome");
+    let endereco = localStorage.getItem("endereco");
+    let email = localStorage.getItem("email");
+    let telefone = localStorage.getItem("telefone");
+
+    let data = "Nome: " + nome +"\n" + "Endereço:" + "\n" + "Email: " + "\n" + "Telefone" + telefone + "\n";
+    const url = "https://api.whatsapp.com/send?phone=" + telefone + "&text" + encodeURIComponent(data);
+
+    window .open(url, "_blank");    
+}
